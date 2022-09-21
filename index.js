a3ca868d33bbef2498b84900d7feed1eb0b716c6
Ecsedi Milan <milan200314@gmail.com>
2022-09-21 09:31:53 +0200

    bmi táblázat logika kijavítva

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
 function szamolas()
 {
-
-   let magassag = parseFloat(document.getElementById('magassag').value);
+    document.getElementById('sovany').classList.remove('kijeloles');
+    document.getElementById('normal').classList.remove('kijeloles');
+    document.getElementById('tulsuly').classList.remove('kijeloles');
+    document.getElementById('tulsuly_1').classList.remove('kijeloles');
+    document.getElementById('tulsuly_2').classList.remove('kijeloles');
+    document.getElementById('tulsuly_3').classList.remove('kijeloles');
+   
+   
+   
+    let magassag = parseFloat(document.getElementById('magassag').value);
    let suly= parseFloat(document.getElementById('testsuly').value);
     let bmi= suly / ((magassag / 100) ** 2);
    document.getElementById('bmi_ertek').textContent= bmi;
 
-   if(bmi<= 18.5){
+   if(bmi <= 18.5){
 
         document.getElementById('sovany').classList.add('kijeloles');
    }
-   else if(bmi => 18.5 && bmi <= 24.9){
+   else if(bmi >= 18.5 && bmi <= 24.9){
 
     document.getElementById('normal').classList.add('kijeloles');
 
    }
-   else if(bmi =>25 && bmi <= 29.9){
+   else if(bmi >= 25 && bmi <= 29.9){
 
     document.getElementById('tulsuly').classList.add('kijeloles');
 
    }
-   else if(bmi => 30 && bmi<= 34.9){
+   else if(bmi >= 30 && bmi<= 34.9){
 
     document.getElementById('tulsuly_1').classList.add('kijeloles');
 
    }
-   else if(bmi => 35 && bmi<= 39.9){
+   else if(bmi >= 35 && bmi<= 39.9){
 
     document.getElementById('tulsuly_2').classList.add('kijeloles');
 
    }
-   else if(bmi => 40){
+   else if(bmi >= 40){
 
     document.getElementById('tulsuly_3').classList.add('kijeloles');
 
